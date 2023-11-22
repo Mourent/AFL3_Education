587996417549676a84c7b80680b12574efecb2cd
Mourent <jmourent@student.ciputra.ac.id>
2023-11-22 12:15:18 +0700

    Initial Commit

---
+++ AFL3_MobCom/AFL3_MobComApp.swift
@@ -0,0 +1,17 @@
+//
+//  AFL3_MobComApp.swift
+//  AFL3_MobCom
+//
+//  Created by MacBook Pro on 22/11/23.
+//
+
+import SwiftUI
+
+@main
+struct AFL3_MobComApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
