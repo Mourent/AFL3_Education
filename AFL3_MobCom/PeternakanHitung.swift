195aa88c126f1513213b62eabb217656084a1ac6
Mourent <jmourent@student.ciputra.ac.id>
2023-11-26 12:28:40 +0700

    buat Hitung Kambing

---
+++ AFL3_MobCom/PeternakanHitung.swift
@@ -0,0 +1,18 @@
+//
+//  PeternakanHitung.swift
+//  AFL3_MobCom
+//
+//  Created by MacBook Pro on 26/11/23.
+//
+
+import SwiftUI
+
+struct PeternakanHitung: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PeternakanHitung()
+}
