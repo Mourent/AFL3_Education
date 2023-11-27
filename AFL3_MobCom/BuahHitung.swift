7ec22328f055d250035752bd9f2df7086789a38a
Mourent <jmourent@student.ciputra.ac.id>
2023-11-27 23:56:51 +0700

    Ubah Home ke ContentView

---
+++ AFL3_MobCom/BuahHitung.swift
@@ -0,0 +1,18 @@
+//
+//  BuahHitung.swift
+//  AFL3_MobCom
+//
+//  Created by MacBook Pro on 27/11/23.
+//
+
+import SwiftUI
+
+struct BuahHitung: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BuahHitung()
+}
