1913eb2c862310497e13467e1e46913693f374fa
AqiilaSM <ashahbani@student.ciputra.ac.id>
2023-11-27 22:09:25 +0700

    zoya kerja udah

---
+++ AFL3_MobCom/START.swift
@@ -0,0 +1,18 @@
+//
+//  START.swift
+//  AFL3_MobCom
+//
+//  Created by MacBook Pro on 27/11/23.
+//
+
+import SwiftUI
+
+struct START: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    START()
+}
---
+++ AFL3_MobCom/buah.swift
@@ -0,0 +1,18 @@
+//
+//  buah.swift
+//  AFL3_MobCom
+//
+//  Created by MacBook Pro on 27/11/23.
+//
+
+import SwiftUI
+
+struct buah: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    buah()
+}
