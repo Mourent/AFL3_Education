4c53bb084c3f3940994df5aecf236781ad673537
Jeffri Handoyo <jhandoyo@student.ciputra.ac.id>
2023-12-01 03:17:05 +0700

    Gabung Semua bikin alur ganti-ganti view

---
+++ AFL3_MobCom/HewanMasuk.swift
@@ -0,0 +1,18 @@
+//
+//  HewanMasuk.swift
+//  AFL3_MobCom
+//
+//  Created by MacBook Pro on 01/12/23.
+//
+
+import SwiftUI
+
+struct HewanMasuk: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HewanMasuk()
+}
