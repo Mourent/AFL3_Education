a3105f7bd8280eeaa42548f472164cf23de96578
Mourent <jmourent@student.ciputra.ac.id>
2023-12-13 15:13:53 +0700

    Buat view baru(not done)

---
+++ AFL3_MobCom/KelinciCocok.swift
@@ -0,0 +1,18 @@
+//
+//  KelinciCocok.swift
+//  AFL3_MobCom
+//
+//  Created by MacBook Pro on 07/12/23.
+//
+
+import SwiftUI
+
+struct KelinciCocok: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    KelinciCocok()
+}
